36d2dc5868188829e0adca6ad8bd240ae904228c
SujithKumar <sujithonweb@gmail.com>
2017-01-26 15:53:20 -0500

    Version 2

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ bot.on('contactRelationUpdate', function(message) {
         var name = message.user ? message.user.name : null;
         var reply = new builder.Message()
             .address(message.address)
-            .text("Hello %s... Thanks for adding me. Say 'hello' to start the conversation", name || 'there');
+            .text("Hello %s... Thanks for adding me. I am not very chatty (I won’t answer things like “How are you?”) but I does handle most of your enquiries. Opening up a chat window with me even brings up the most common help issues to make the process even more convenient. Say 'hello' to start the conversation", name || 'there');
         bot.send(reply);
     } else {
         // delete their data
@@ -331,7 +331,7 @@ bot.dialog('/Disbursements', [
 
 bot.dialog('/Illustrations', [
     function(session) {
-        session.send("You can pass a custom message to Prompts.choice() that will present the user with a carousel of cards to select from. Each card can even support multiple actions.");
+        //session.send("You can pass a custom message to Prompts.choice() that will present the user with a carousel of cards to select from. Each card can even support multiple actions.");
 
         // Ask the user to select an item from a carousel.
         var msg = new builder.Message(session)
@@ -339,36 +339,46 @@ bot.dialog('/Illustrations', [
             .attachmentLayout(builder.AttachmentLayout.carousel)
             .attachments([
                 new builder.HeroCard(session)
-                .title("Space Needle")
-                .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                .title("Payments")
+                .text("Payment Related Queries")
                 .images([
-                    builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                    .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/800px-Seattlenighttimequeenanne.jpg")),
+                    builder.CardImage.create(session, "https://v9zyuw-sn3301.files.1drv.com/y3mE7EmeBZ6Ta_R5dierdYdLp5BtEEn5SB49lTROsZxsQBBS_wTggS2VpQgS1xQZyI86MC52a69zlJV-9pjrRDi3XImLTl84T_1v09Bk7dkyR9j2RccmdfjD-qTIoL3v-doPYkLBnP5qldaYR1lB7z6Oc5zEB78DPBTzrYsRvvMIFg?width=89&height=94&cropmode=none")
+                    .tap(builder.CardAction.showImage(session, "https://v9zyuw-sn3301.files.1drv.com/y3mE7EmeBZ6Ta_R5dierdYdLp5BtEEn5SB49lTROsZxsQBBS_wTggS2VpQgS1xQZyI86MC52a69zlJV-9pjrRDi3XImLTl84T_1v09Bk7dkyR9j2RccmdfjD-qTIoL3v-doPYkLBnP5qldaYR1lB7z6Oc5zEB78DPBTzrYsRvvMIFg?width=89&height=94&cropmode=none")),
                 ])
                 .buttons([
-                    builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle", "Wikipedia"),
-                    builder.CardAction.imBack(session, "select:100", "Select")
+                    // builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle", "Wikipedia"),
+                    builder.CardAction.imBack(session, "Payments", "Select")
                 ]),
                 new builder.HeroCard(session)
-                .title("Pikes Place Market")
-                .text("<b>Pike Place Market</b> is a public market overlooking the Elliott Bay waterfront in Seattle, Washington, United States.")
+                .title("Claims")
+                .text("<b>Claims</b> Claims related Queries")
                 .images([
-                    builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg")
+                    builder.CardImage.create(session, "https://v9zvuw-sn3301.files.1drv.com/y3mFgbroj_cV5AEPfvuHLoOpO5cCYDKAndBUn1h14vmI27i1d82fAoPGfuY6MjZSQlUXsVtXOrK28KLAJGw29vAWfc9yoANebp7yhID-K4nqtPgFTIuyD6FhRlmPww5yIy7oLKSxzTYWWssYlNYgase8EdeHILveI9Ywi9ZKG7nqy4?width=98&height=91&cropmode=none")
                     .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/800px-PikePlaceMarket.jpg")),
                 ])
                 .buttons([
-                    builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Pike_Place_Market", "Wikipedia"),
-                    builder.CardAction.imBack(session, "select:101", "Select")
+                    // builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Pike_Place_Market", "Wikipedia"),
+                    builder.CardAction.imBack(session, "Claims", "Select")
+                ]),
+                new builder.HeroCard(session)
+                .title("Contact Us")
+                .text("<b>Pike Place Market</b> is a public market overlooking the Elliott Bay waterfront in Seattle, Washington, United States.")
+                .images([
+                    builder.CardImage.create(session, "https://wnzouw-sn3301.files.1drv.com/y3mT9AqmAqeXgSkNUaIOlFgkq9_fJ-c654C172n1AwUWeQfzdMmcLBP-JOtHLmrJaQCPTtBKNuyIsvPinxzJJxMG1NBpTgneKn-Ej4gAgGb4JMwaf1va2S1sozEKXZ-A9pQW-_pP4KW17_SKmxFwkTF_bK9VN_kgN1wk_vE7Zox9dg?width=94&height=94&cropmode=none")
+                    .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/800px-PikePlaceMarket.jpg")),
+                ])
+                .buttons([
+                    builder.CardAction.openUrl(session, "http://foresters.com", "Foresters.com"),
+                    builder.CardAction.imBack(session, "ContactUS", "Select")
                 ]),
                 new builder.HeroCard(session)
                 .title("EMP Museum")
                 .text("<b>EMP Musem</b> is a leading-edge nonprofit museum, dedicated to the ideas and risk-taking that fuel contemporary popular culture.")
                 .images([
-                    builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/320px-Night_Exterior_EMP.jpg")
+                    builder.CardImage.create(session, "https://wnznuw-sn3301.files.1drv.com/y3mjUlQnXj3KQZo2Uu42sv6mcs3fkudi9TBj_FTnAETyFxma9SiVjROj5Vqqpqk7bcP2iUZnUK-_RR_VcKl-QRo0CNFKqoMJBXOHo7yW6z5Ui-z_szdnyFkcHmW3OHKb2iRv0zgUWyR3Z1J4l8W2v0Xv1rCyKsTkFT4SQiD0TBh--o?width=94&height=94&cropmode=none")
                     .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/800px-Night_Exterior_EMP.jpg"))
                 ])
                 .buttons([
-                    builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/EMP_Museum", "Wikipedia"),
                     builder.CardAction.imBack(session, "select:102", "Select")
                 ])
             ]);
@@ -377,20 +387,19 @@ bot.dialog('/Illustrations', [
     function(session, results) {
         var action, item;
         var kvPair = results.response.entity.split(':');
-        switch (kvPair[0]) {
-            case 'select':
-                action = 'selected';
+
+        switch (kvPair) {
+            case 'Payments':
+                item = "the <b>Payments</b>";
                 break;
-        }
-        switch (kvPair[1]) {
-            case '100':
-                item = "the <b>Space Needle</b>";
+            case 'Claims':
+                item = "<b>Claims</b>";
                 break;
-            case '101':
-                item = "<b>Pikes Place Market</b>";
+            case 'ContactUs':
+                item = "ContactUs";
                 break;
-            case '102':
-                item = "the <b>EMP Museum</b>";
+            case 'Quit':
+                item = "Quit";
                 break;
         }
         session.endDialog('You %s "%s"', action, item);
